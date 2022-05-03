95a8d6b631a8e25a0adabc76a3c067282b5bfd25
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-03 13:08:09 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App bg-orange-500">
-     
-      <h2>Shri krishnaya Namaha</h2>
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-
+        <div className='flex justify-between items-center'>
+            <h1 className='p-3'>Shopping Cart</h1>
+            <div className='flex items-center'>
+             <FaShoppingCart/> <span className='mr-10 ml-2'>0</span>
+            </div>
+        </div>
     </>
   )
 }
