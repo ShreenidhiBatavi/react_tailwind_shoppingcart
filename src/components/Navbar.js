0be7e7cc193c7f2c67d9af9cebeaef2956e3b7f2
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-04 12:00:03 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-        <div className='flex justify-between items-center'>
-            <h1 className='p-3'>Shopping Cart</h1>
+        <div className='flex justify-between items-center bg-red-500'>
+            <h1 className='p-3 text-white text-xl'>Shopping Cart</h1>
             <div className='flex items-center'>
-             <FaShoppingCart/> <span className='mr-10 ml-2'>0</span>
+             <FaShoppingCart /> <span className='mr-10 ml-2 text-xl text-white font-bold'>0</span>
             </div>
         </div>
     </>
