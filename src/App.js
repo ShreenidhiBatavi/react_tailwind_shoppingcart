0d2fe63ed95e6c304dd95d7b3fa39d058304fc6e
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-06 22:23:44 +0530

    changes for app component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { GlobalProvider } from './context/globalState';
 import Products from './components/Products';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SingleProduct from './components/SingleProduct';
+import Cart from './components/Cart';
 function App() {
   return (
     <>
       <GlobalProvider>
         <div className='bg-gray-600'>
-          <Navbar />
+         
           <Router>
+            <Navbar />
             <Routes>
               <Route path="/" element={<Products />} />
               <Route path="/singleProduct" element={<SingleProduct />} />
+              <Route path="/cart" element={<Cart/>}/>
             </Routes>
           </Router>
 
