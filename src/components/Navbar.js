4584a62025abdc26ab0ce4b9d59a120a60c22e3e
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-06 22:25:06 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { ContextProvider } from '../context/globalState';
 const Navbar = () => {
+  const {cart}=ContextProvider()
+  console.log(cart)
   return (
     <>
-        <div className='flex justify-between items-center bg-red-500'>
-            <h1 className='p-3 text-white text-xl'>Shopping Cart</h1>
+        <div className='flex justify-between items-center bg-red-500 fixed mb-16 left-0 w-full top-0'>
+            <h1 className='p-3 text-white text-xl ml-4'>Shopping Cart</h1>
+            <Link to="/cart">
             <div className='flex items-center'>
-             <FaShoppingCart color='white' /> <span className='mr-10 ml-2 text-xl text-white font-bold'>0</span>
+             <FaShoppingCart color='white' /> <span className='mr-10 ml-2 text-xl text-black font-bold bg-white w-8 rounded-full flex justify-center items-center '>{cart.length}</span>
             </div>
+            </Link>
+            
         </div>
     </>
   )
