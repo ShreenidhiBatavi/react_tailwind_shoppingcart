e61fd9171cb4c5244bf8656debf3848a4821956d
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-07 15:55:00 +0530

    changes to prod comp

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,31 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { ContextProvider } from '../context/globalState'
-const Product = () => {
-    const {products}=ContextProvider()
+const Product = ({product}) => {
     
-  
-  return (
+    const navigate=useNavigate()
+    const {cart,setCart}=ContextProvider()
+    
+    const addToCart=(product)=>{
+        setCart([...cart,product])
+    }
+   return (
     <>
-        <div className='bg-blue-300 grid lg:grid-cols-4 justify-center items-center'>
-            {products.map((prod)=>(
-                <>
-                    <div className='bg-orange-200 h-72'>
-                        <img className="w-full h-42"src={prod.image}/>
-                         <h1>{prod.title}</h1>
-                         <h2>{prod.price}</h2>
-                    </div>
-                </>
-            ))}
-        </div>
-    </>
+       <div className='shadow-xl bg-white p-2 lg:mx-0 mx-12 rounded-lg'>
+            <img className='object-scale-down h-52 w-full mt-5' src={product.image}/>
+            <h1 className='mt-2 p-4 font-bold'>{product.title}</h1>
+            <h2 className=' p-4 font-bold'>Price  {product.price} RS</h2>
+            <div className='flex justify-between p-2'>
+                <button onClick={()=>navigate('/singleProduct',{state:product})} className='w-40 h-10 p-1 bg-red-500 text-white rounded mx-1 hover:bg-red-600 hover:font-bold'>
+                    View
+                </button>
+            
+                 <button onClick={()=>addToCart(product)} className='w-40 p-1 h-10 bg-red-500 text-white rounded mx-1 hover:bg-red-60 hover:font-bold'> 
+                        Add to cart
+                  </button>
+             </div>
+         </div>
+     </>
   )     
 }
 
