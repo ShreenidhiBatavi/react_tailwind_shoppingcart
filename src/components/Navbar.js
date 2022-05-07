bcce078d5a09f27b51fcf1fabe0b170a17b18239
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-07 15:56:16 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <>
         <div className='flex justify-between items-center bg-red-500 fixed mb-16 left-0 w-full top-0'>
-            <h1 className='p-3 text-white text-xl ml-4'>shopping Cart</h1>
+            <h1 className='p-3 text-white text-xl ml-4'>Shopping Cart</h1>
             <Link to="/cart">
             <div className='flex items-center'>
              <FaShoppingCart color='white' /> <span className='mr-10 ml-2 text-xl text-black font-bold bg-white w-8 rounded-full flex justify-center items-center '>{cart.length}</span>
