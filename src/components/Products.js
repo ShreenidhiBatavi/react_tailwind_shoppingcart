23bc5b74977796529e05da99991a78f4455973ef
shreenidhi Batavi <batavishrinidhi@gmail.com>
2022-05-18 16:40:25 +0530

    producs componentt added

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { ContextProvider } from '../context/globalState'
 import Product from './Product'
+import ReactLoading from 'react-loading';
 const Products = () => {
     const {products}=ContextProvider()
     console.log(products)
   return (
     <>
-        <div className=' grid lg:grid-cols-4 container mx-auto mt-12 gap-10 justify-center items-center'>
+       {products.length?<div className=' grid lg:grid-cols-4 container mx-auto mt-12 gap-10 justify-center items-center'>
             {products.map((product)=>(
                 <>
                     <Product key={product.id} product={product}/>
                 </>
             ))}
         </div>
+        :
+        <div className='flex justify-center items-center h-screen'>
+            <ReactLoading type="spokes" color="white"  height={100} width={100}/>
+        </div>}
     </>
   )
 }
